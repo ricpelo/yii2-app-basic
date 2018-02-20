07b0a3efaa4624cda3f3f56b68609124f57751b0
Ricardo Pérez <ricpelo@gmail.com>
2018-02-20 13:38:24 +0100

    Rellena la BD de pruebas en cada test

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 DIR=$(basename $(realpath .))
 
-sed -i s/proyecto/$DIR/g db/* config/* proyecto.conf
+sed -i s/proyecto/$DIR/g db/* config/* proyecto.conf codeception.yml
 mv db/proyecto.sql db/$DIR.sql
+mv tests/_data/proyecto.sql tests/_data/$DIR.sql
 mv proyecto.conf $DIR.conf
 rm -f $0
