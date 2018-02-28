50673e220a5596a933954f095f2ccf1749413fe3
Ricardo Pérez <ricpelo@gmail.com>
2018-02-28 21:00:00 +0100

    Corrige enlace simbólico a db/proyecto.sql

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 DIR=$(basename $(realpath .))
 
 sed -i s/proyecto/$DIR/g db/* config/* proyecto.conf codeception.yml
+mv proyecto.conf $DIR.conf
 mv db/proyecto.sql db/$DIR.sql
 mv tests/_data/proyecto.sql tests/_data/$DIR.sql
-mv proyecto.conf $DIR.conf
+ln -sf ../../db/$DIR.sql tests/_data/$DIR.sql
 rm -f $0
