2bbf96c141325130e3ad2440b5ed7187f69f1c88
Ricardo Pérez <ricpelo@gmail.com>
2018-12-13 12:07:30 +0100

    Corrige db/create.sh y db/load.sh

--- db/create.sh
+++ db/create.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-if [ "$1" = "travis" ]
-then
+if [ "$1" = "travis" ]; then
     psql -U postgres -c "CREATE DATABASE proyecto_test;"
     psql -U postgres -c "CREATE USER proyecto PASSWORD 'proyecto' SUPERUSER;"
 else
-    [ "$1" != "test" ] && sudo -u postgres dropdb --if-exists proyecto
-    [ "$1" != "test" ] && sudo -u postgres dropdb --if-exists proyecto_test
-    [ "$1" != "test" ] && sudo -u postgres dropuser --if-exists proyecto
+    sudo -u postgres dropdb --if-exists proyecto
+    sudo -u postgres dropdb --if-exists proyecto_test
+    sudo -u postgres dropuser --if-exists proyecto
     sudo -u postgres psql -c "CREATE USER proyecto PASSWORD 'proyecto' SUPERUSER;"
-    [ "$1" != "test" ] && sudo -u postgres createdb -O proyecto proyecto
+    sudo -u postgres createdb -O proyecto proyecto
+    sudo -u postgres psql -d proyecto -c "CREATE EXTENSION pgcrypto;" 2>/dev/null
     sudo -u postgres createdb -O proyecto proyecto_test
+    sudo -u postgres psql -d proyecto_test -c "CREATE EXTENSION pgcrypto;" 2>/dev/null
     LINE="localhost:5432:*:proyecto:proyecto"
     FILE=~/.pgpass
-    if [ ! -f $FILE ]
-    then
+    if [ ! -f $FILE ]; then
         touch $FILE
         chmod 600 $FILE
     fi
-    if ! grep -qsF "$LINE" $FILE
-    then
+    if ! grep -qsF "$LINE" $FILE; then
         echo "$LINE" >> $FILE
     fi
 fi
