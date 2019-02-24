7c031f877090a8b7caf1f465ad7bc56a39e37ef5
Ricardo Pérez <ricpelo@gmail.com>
2019-02-24 12:09:00 +0100

    Corrige pruebas, no usar Db de Codeception

--- setup.sh
+++ setup.sh
@@ -5,6 +5,4 @@ DIR=$(basename $(realpath .))
 sed -i s/proyecto/$DIR/g db/* config/* proyecto.conf codeception.yml
 mv proyecto.conf $DIR.conf
 mv db/proyecto.sql db/$DIR.sql
-mv tests/_data/proyecto.sql tests/_data/$DIR.sql
-ln -sf ../../db/$DIR.sql tests/_data/$DIR.sql
 rm -f $0
