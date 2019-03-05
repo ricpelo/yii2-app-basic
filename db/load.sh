bf52cf091a4c09206a2aca7e641851858966d1ea
Ricardo Pérez <ricpelo@gmail.com>
2019-03-05 12:48:38 +0100

    Corrige fallo de espacios en rutas en db/load.sh

--- db/load.sh
+++ db/load.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE_DIR=$(dirname $(readlink -f "$0"))
+BASE_DIR=$(dirname "$(readlink -f "$0")")
 if [ "$1" != "test" ]; then
     psql -h localhost -U proyecto -d proyecto < $BASE_DIR/proyecto.sql
 fi
